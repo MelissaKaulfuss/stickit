0f0ae8ebd0179ac64c431f65616c762d351c4a98
MelissaKaulfuss <melissacor3lfuss@gmail.com>
2018-01-25 17:00:18 +1100

    Change Schema so that all User fields are not null

--- db/migrate/20180123050321_create_users.rb
+++ db/migrate/20180123050321_create_users.rb
@@ -1,9 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :email
-      t.integer :status
+      t.string :username, null: false
+      t.string :email, null: false
 
       t.timestamps
     end
--- db/migrate/20180123054802_remove_status_from_user.rb
+++
@@ -1,5 +0,0 @@
-class RemoveStatusFromUser < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :users, :status, :integer
-  end
-end
