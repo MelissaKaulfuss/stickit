212d55107647958d65014d3cdfc3659437366278
MelissaKaulfuss <melissacor3lfuss@gmail.com>
2018-01-26 21:32:35 +1100

    WIP user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,10 @@
+require "spec_helper"
 require "rails_helper"
 
-describe User do
+describe "User" do
  context "validations" do
+    subject { build(:user) }
+
     it { is_expected.to validate_presence_of(:username) }
     it { is_expected.to validate_presence_of(:email) }
   end
