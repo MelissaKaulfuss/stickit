08d7444332801811400be69bf918e29227dcd7dc
MelissaKaulfuss <melissacor3lfuss@gmail.com>
2018-01-27 22:06:57 +1100

    Change format of route for user#new action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new]
 end
