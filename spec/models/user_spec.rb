0b264a14b5e85b008fb3c533abb73da7fe9ed8ce
MelissaKaulfuss <melissacor3lfuss@gmail.com>
2018-01-27 22:02:21 +1100

    WIP re-edit user spec after Friday's change vanished

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,7 @@
-require "spec_helper"
 require "rails_helper"
 
-describe "User" do
+describe User do
  context "validations" do
-    subject { build(:user) }
 
     it { is_expected.to validate_presence_of(:username) }
     it { is_expected.to validate_presence_of(:email) }
