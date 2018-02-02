92f6d23a1434eb7e550757b53fd9ee65326c5c58
MelissaKaulfuss <melissacor3lfuss@gmail.com>
2018-02-02 15:41:00 +1100

    [WIP] Rubocop specific style edits

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -164,12 +164,12 @@ SimpleForm.setup do |config|
   # config.input_class = nil
 
   # Define the default class of the input wrapper of the boolean input.
-  config.boolean_label_class = 'checkbox'
+  config.boolean_label_class = "checkbox"
 
   # Defines if the default input wrapper class should be included in radio
   # collection wrappers.
   # config.include_default_input_wrapper_class = true
 
   # Defines which i18n scope will be used in Simple Form.
-  # config.i18n_scope = 'simple_form'
+  # config.i18n_scope = 'simple_form"
 end
