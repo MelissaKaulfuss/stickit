24b0eba76c8959a1f61781c17558d73daa808acc
MelissaKaulfuss <melissacor3lfuss@gmail.com>
2018-02-02 15:41:46 +1100

    [WIP] Rubocop specific style edits

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,8 +1,6 @@
 require "rails_helper"
-
 feature "User creates an account" do
   scenario "with a valid username, email and password" do
-
     visit new_user_path
 
     fill_in "Username", with: "felixpip"
