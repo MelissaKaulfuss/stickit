263c32cb7d0c6249e4ecaabcd565234dcaaad9d9
MelissaKaulfuss <melissacor3lfuss@gmail.com>
2018-02-02 14:40:03 +1100

    User has password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,10 +3,11 @@ require "rails_helper"
 RSpec.describe User do
   subject { build(:user) }
     context "validations" do
+    
     it { is_expected.to validate_presence_of(:username) }
     it { is_expected.to validate_presence_of(:email) }
-    it { is_expected.to validate_presence_of(:password) }
     it { is_expected.to validate_uniqueness_of(:username) }
-    it { is_expected.to validate_uniqueness_of(:password) } 
- end
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to have_secure_password }
+   end
 end 
