0adbddb5005c97dc80517b1a90bde7bff6643c7a
MelissaKaulfuss <melissacor3lfuss@gmail.com>
2018-02-02 14:46:44 +1100

    Change to User Factory password field

--- spec/factories/factory.rb
+++ spec/factories/factory.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     username "felix_pip"
-    email "felix@pip.com"
-    password_digest "secret"
+    email  "felix@email.com"
+    password "password"
   end
 end
