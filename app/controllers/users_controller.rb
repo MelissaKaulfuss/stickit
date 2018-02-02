d8674eaeebae47b46ea915b173bc65eb4c92cf94
MelissaKaulfuss <melissacor3lfuss@gmail.com>
2018-02-02 14:39:00 +1100

    Add show controller action with tests for User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,27 @@ class UsersController < ApplicationController
 
   def create
    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      respond_with(@user)
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   private
 
+  helper_method :user
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
+
+  def user
+    @user ||= User.find(params[:id])
+  end
+
 end
--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -3,27 +3,26 @@ require "rails_helper"
 feature "User creates an account" do
   scenario "with a valid username, email and password" do
 
-    visit "users/new"
+    visit new_user_path
 
     fill_in "Username", with: "felixpip"
     fill_in "Email", with: "example@eg.com"
     fill_in "Password", with: "password"
-    click_button  "Sign up"
-    
-    expect(page).to_have content("Welcome, you've created your account!")
+    click_button "Create User"
+
+    expect(page).to have_text("welcome to stickit felixpip!")
   end
 
-  scenario "with an invalid email" do
-    
-    visit "users/new"
+  feature "User sees their account details" do
+    scenario "when signing up with a valid username, email and password" do
+      
+      user = create(:user, username: "billybob", email: "well@howdydudey.com")
 
-    fill_in "Username", with: "felixpip"
-    fill_in "Email", with: "example"
-    fill_in "Password", with: "password"
-    click_button "Sign up"
+      visit user_path(user)
 
-    expect(page).to_have content("Invalid details - try again!")
-  end
+      expect(page).to have_text("welcome to stickit billybob!")
+    end
+  end  
 end
 
 
