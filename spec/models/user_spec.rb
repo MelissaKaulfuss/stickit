21021d99825c4efda7070592a210e92c50250d05
MelissaKaulfuss <melissacor3lfuss@gmail.com>
2018-02-09 22:27:08 +1100

    Fix layout of spec and tests uniqueness of email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,10 +3,11 @@ require "rails_helper"
 RSpec.describe User do
   context "validations" do
     subject { build(:user) }
+
     it { is_expected.to validate_presence_of(:username) }
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_uniqueness_of(:username) }
-    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_uniqueness_of(:email) }
     it { is_expected.to have_secure_password }
   end
 end
