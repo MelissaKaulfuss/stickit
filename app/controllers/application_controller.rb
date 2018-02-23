06bbe4958eda07a0c8ee6ea1840cfc20eee6e674
MelissaKaulfuss <melissacor3lfuss@gmail.com>
2018-02-23 15:51:37 +1100

    fixup! User can sign in and log out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 
   def current_user
     if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+      @current_user ||= User.find(session[:user_id])
     end
   end
   helper_method :current_user
