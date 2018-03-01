862daa9931bd7a2a9bde942fc9cc67265e944177
MelissaKaulfuss <melissacor3lfuss@gmail.com>
2018-03-01 21:27:05 +1100

    fixup! User can sign in and log out

---
+++ spec/support/features/authentication_helpers.rb
@@ -0,0 +1,14 @@
+module Features
+  module AuthenticationHelpers
+
+    def login_as(user)
+      visit new_session_path
+      within "form" do
+        fill_in "session_email", with: user.email
+        fill_in "session_password", with: user.password
+        click_on I18n.t("sessions.new.submit")
+      end
+
+    end
+  end
+end
