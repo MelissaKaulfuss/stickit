6066e7031bcb8ce74acae5ef7d27f71dc4fcf3f7
MelissaKaulfuss <melissacor3lfuss@gmail.com>
2018-04-26 16:38:54 +1000

    Rubocop requested fixes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,7 @@
 # This file is copied to spec/ when you run "rails generate rspec:install"
 require "spec_helper"
 ENV["RAILS_ENV"] ||= "test"
-require File.expand_path("../../config/environment", __FILE__)
+require File.expand_path("../config/environment", __dir__)
 abort("The Rails environment is running in prod mode!") if Rails.env.production?
 require "rspec/rails"
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
